0d2e850451bc37b3dfd8c001274e0d84a5fbba24
Morten Lang <lycilph@gmail.com>
2018-04-17 08:16:12 +0200

    Working on the trainer

---
+++ Trainer/CreateDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Trainer
+{
+    public partial class CreateDialog
+    {
+        private Regex regex = new Regex("[^0-9]+");
+
+        public int PopulationSize
+        {
+            get { return (int)GetValue(PopulationSizeProperty); }
+            set { SetValue(PopulationSizeProperty, value); }
+        }
+        public static readonly DependencyProperty PopulationSizeProperty = DependencyProperty.Register("PopulationSize", typeof(int), typeof(CreateDialog), new PropertyMetadata(200));
+
+        public int SimulationRuns
+        {
+            get { return (int)GetValue(SimulationRunsProperty); }
+            set { SetValue(SimulationRunsProperty, value); }
+        }
+        public static readonly DependencyProperty SimulationRunsProperty = DependencyProperty.Register("SimulationRuns", typeof(int), typeof(CreateDialog), new PropertyMetadata(5));
+
+        public CreateDialog()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void OkClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void ValidateNumericInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
--- Trainer/MainWindow.xaml.cs
+++ Trainer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using OxyPlot;
 using OxyPlot.Series;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace Trainer
         private LineSeries fitness_series;
         private IProgress<int> progress;
         private CancellationTokenSource cts;
+        private List<Individual> population;
 
         public PlotModel Model
         {
@@ -43,7 +45,14 @@ namespace Trainer
             get { return (int)GetValue(PopulationSizeProperty); }
             set { SetValue(PopulationSizeProperty, value); }
         }
-        public static readonly DependencyProperty PopulationSizeProperty = DependencyProperty.Register("PopulationSize", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+        public static readonly DependencyProperty PopulationSizeProperty = DependencyProperty.Register("PopulationSize", typeof(int), typeof(MainWindow), new PropertyMetadata(1000));
+
+        public int SimulationRuns
+        {
+            get { return (int)GetValue(SimulationRunsProperty); }
+            set { SetValue(SimulationRunsProperty, value); }
+        }
+        public static readonly DependencyProperty SimulationRunsProperty = DependencyProperty.Register("SimulationRuns", typeof(int), typeof(MainWindow), new PropertyMetadata(5));
 
         public int Generation
         {
@@ -65,9 +74,7 @@ namespace Trainer
             DataContext = this;
 
             Model = new PlotModel { Title = "Statistics" };
-
             fitness_series = new LineSeries { Title = "Fitness" };
-            fitness_series.Points.Add(new DataPoint(0, 0));
             Model.Series.Add(fitness_series);
 
             progress = new Progress<int>(i => 
@@ -75,53 +82,155 @@ namespace Trainer
                 Counter += i;
                 if (Counter > PopulationSize)
                     Counter = 0;
-
-                Messages.Insert(0, $"Counter {Counter}");
-                if (Messages.Count > 15)
-                    Messages.RemoveAt(Messages.Count - 1);
             });
 
-            Counter = 0;
-            PopulationSize = 100;
-            Generation = 1;
             Messages = new ObservableCollection<string>();
+
+            Reset();
+        }
+
+        private void AddMessage(string msg)
+        {
+            Messages.Insert(0, msg);
+            if (Messages.Count > 15)
+                Messages.RemoveAt(Messages.Count - 1);
+        }
+
+        private void Reset()
+        {
+            Messages.Clear();
+            Counter = 0;
+            Generation = 1;
+
+            fitness_series.Points.Clear();
+            fitness_series.Points.Add(new DataPoint(0, 0));
+            Model.InvalidatePlot(true);
+
+            population = Enumerable.Range(0, PopulationSize).Select(_ => new Individual()).ToList();
+        }
+
+        private void CreateClick(object sender, RoutedEventArgs e)
+        {
+            var create_dialog = new CreateDialog
+            {
+                Owner = this,
+                PopulationSize = PopulationSize,
+                SimulationRuns = SimulationRuns
+            };
+
+            var result = create_dialog.ShowDialog();
+            if (result == true)
+            {
+                PopulationSize = create_dialog.PopulationSize;
+                SimulationRuns = create_dialog.SimulationRuns;
+                Reset();
+            }
+        }
+
+        private void LoadClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, "Not implemented yet", "Information");
         }
 
         private void StartClick(object sender, RoutedEventArgs e)
         {
             Running = true;
             cts = new CancellationTokenSource();
-
-            Counter = 0;
-            PopulationSize = 200;
-            var population = Enumerable.Range(0, PopulationSize).Select(_ => new Individual()).ToList();
-
             var scheduler = TaskScheduler.FromCurrentSynchronizationContext();
-            Task.Run(() =>
-            {
-                try
-                {
-                    var po = new ParallelOptions { CancellationToken = cts.Token };
-                    Parallel.ForEach(population, po, i =>
-                    {
-                        i.Simulate(5);
-                        progress.Report(1);
-                        Thread.Sleep(100);
-                    });
+            var simulation_runs = SimulationRuns;
 
-                }
-                catch (Exception)
-                {
-                    Debug.WriteLine("Simulation was cancelled");
-                }
-            })
-            .ContinueWith(a => 
+            Task.Run(()=> 
             {
-                fitness_series.Points.Add(new DataPoint(Generation, rnd.NextDouble() * 10.0));
-                Running = false;
-                Generation++;
-                Model.InvalidatePlot(true);
-            }, scheduler);
+                var sw = new Stopwatch();
+
+                while (!cts.IsCancellationRequested)
+                {
+                    sw.Restart();
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        AddMessage($"Starting generation {Generation}");
+                    }, CancellationToken.None, TaskCreationOptions.None, scheduler);
+
+                    // Simulate all individuals in a population
+                    try
+                    {
+                        var po = new ParallelOptions { CancellationToken = cts.Token };
+                        Parallel.ForEach(population, po, i =>
+                        {
+                            i.Simulate(simulation_runs);
+                            progress.Report(1);
+                        });
+
+                    }
+                    catch (Exception)
+                    {
+                        Debug.WriteLine("Simulation was cancelled");
+                    }
+
+                    // Order population by fitness
+                    var ordered_population = population.OrderByDescending(i => i.fitness).ToList();
+
+                    sw.Stop();
+                    var elapsed = sw.ElapsedMilliseconds;
+                    Task.Factory.StartNew(() =>
+                    {
+                        AddMessage($"Generation {Generation} took {elapsed} ms");
+                        AddMessage($"Maximum fitness {ordered_population.First().fitness}");
+
+                        fitness_series.Points.Add(new DataPoint(Generation, ordered_population.First().fitness));
+                        Model.InvalidatePlot(true);
+
+                        Generation++;
+                        Counter = 0;
+                    }, CancellationToken.None, TaskCreationOptions.None, scheduler)
+                    .Wait();
+                }
+            });
+
+            //Counter = 0;
+            //PopulationSize = 200;
+            //var population = Enumerable.Range(0, PopulationSize).Select(_ => new Individual()).ToList();
+
+            //var scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            //Task.Run(() =>
+            //{
+            //    var sw = Stopwatch.StartNew();
+
+            //    Task.Factory.StartNew(() =>
+            //    {
+            //        AddMessage($"Starting generation {Generation}");
+            //    }, CancellationToken.None, TaskCreationOptions.None, scheduler);
+
+            //    // Simulate all individuals in a population
+            //    try
+            //    {
+            //        var po = new ParallelOptions { CancellationToken = cts.Token };
+            //        Parallel.ForEach(population, po, i =>
+            //        {
+            //            i.Simulate(5);
+            //            progress.Report(1);
+            //        });
+
+            //    }
+            //    catch (Exception)
+            //    {
+            //        Debug.WriteLine("Simulation was cancelled");
+            //    }
+
+            //    sw.Stop();
+            //    Task.Factory.StartNew(() =>
+            //    {
+            //        AddMessage($"Generation {Generation} took {sw.ElapsedMilliseconds} ms");
+            //    }, CancellationToken.None, TaskCreationOptions.None, scheduler);
+            //})
+            //.ContinueWith(a => 
+            //{
+            //    fitness_series.Points.Add(new DataPoint(Generation, rnd.NextDouble() * 10.0));
+            //    Running = false;
+            //    Generation++;
+            //    Model.InvalidatePlot(true);
+            //}, scheduler);
         }
 
         private void StopClick(object sender, RoutedEventArgs e)
